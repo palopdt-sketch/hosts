c65b6bb80b1a55470c0485901610bfe58a3fa6e4
palopdt-sketch <palopdt@gmail.com>
2026-09-21 18:53:50 +0700

    Create tampermonkey.js

---
+++ tampermonkey.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         A.I Script
+// @namespace    https://github.com/palopdt-sketch
+// @version      1.0.1
+// @description  A.I Script
+// @match        *://*/
+// @match        *://*/*
+// @updateURL    https://raw.githubusercontent.com/USERNAME/REPO/main/script.user.js
+// @downloadURL  https://raw.githubusercontent.com/USERNAME/REPO/main/script.user.js
+// @connect     *
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+
+(function () {
+    alert("test2");
+})();
